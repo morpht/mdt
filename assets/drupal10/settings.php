40097eb792ecf001e9e97c071f2ec2e9b8a82d93
Radim Klaška <radim@morpht.com>
2023-10-10 06:06:43 +0200

    mdt-34486: Remove duplicate settings.local.php include. (#7)
    Local settings is already included here: assets/drupal10/settings.main.php:118.

--- assets/drupal10/settings.php
+++ assets/drupal10/settings.php
@@ -10,8 +10,4 @@ $site = 'default';
 // Include site specific settings.
 require DRUPAL_ROOT . "/sites/$site/settings/settings.main.php";
 
-if (file_exists(DRUPAL_ROOT . "/sites/$site/settings/settings.local.php")) {
-  include DRUPAL_ROOT . "/sites/$site/settings/settings.local.php";
-}
-
 /* Drupal installation will put hash salt here. Remove it manually. */
